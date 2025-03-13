19c12853fb49ade2fbc37dfe2336279458abe594
buhaistrikalo <kirilltcoi@yandex.ru>
2025-03-13 20:35:50 +0300

    Изменена проверка авторизации в маршруте регистрации для возврата результата функции guestGuard. Обновлен элемент контейнера с div на main для семантической разметки.

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -3,20 +3,18 @@ import { RegisterForm } from "@/components/register-form";
 import { guestGuard } from "@/lib/auth-context";
 
 export const Route = createFileRoute("/register")({
-  // Добавляем проверку, что пользователь не авторизован
   beforeLoad: () => {
-    // Проверяем, что пользователь не авторизован
-    guestGuard();
+    return guestGuard();
   },
   component: RouteComponent,
 });
 
 function RouteComponent() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <RegisterForm />
       </div>
-    </div>
+    </main>
   );
 }
